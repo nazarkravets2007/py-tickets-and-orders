3d7fed8c55d351062d7b533898a764c0fdabc7b7
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-02-11 15:12:19 +0200

    Update cinema_hall.py

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -1,15 +1,24 @@
+from django.db import transaction
 from django.db.models import QuerySet
-
 from db.models import CinemaHall
 
 
 def get_cinema_halls() -> QuerySet:
+    """
+    Повертає queryset всіх кінозалів
+    """
     return CinemaHall.objects.all()
 
 
+@transaction.atomic
 def create_cinema_hall(
     hall_name: str, hall_rows: int, hall_seats_in_row: int
 ) -> CinemaHall:
+    """
+    Створює новий CinemaHall в транзакції
+    """
     return CinemaHall.objects.create(
-        name=hall_name, rows=hall_rows, seats_in_row=hall_seats_in_row
+        name=hall_name,
+        rows=hall_rows,
+        seats_in_row=hall_seats_in_row
     )
